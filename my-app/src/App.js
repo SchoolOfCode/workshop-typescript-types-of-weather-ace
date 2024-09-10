956094414e42334885b65c0b0353d5ec01c28b83
4rchie-r <archiereader@outlook.com>
2024-09-10 13:31:30 +0100

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://bxxqiouywboyonogpvsu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4eHFpb3V5d2JveW9ub2dwdnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjUyMzksImV4cCI6MjA0MTU0MTIzOX0.BKNW7qSLn2d-9NwN7CYeZ2o8RLxvFV38Q7YgSUtb1jA')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+}
